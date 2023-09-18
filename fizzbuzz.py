b2415274580a03352e5c3af07a7e7b1a4c241cee
ardor924 <ardor924@gmail.com>
2023-09-18 15:12:28 +0900

    print buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,11 @@
-def fizzbuzz(num:int) :
+def buzz(num:int) :
     for i in range(1,num+1) :
-        if i == 15 :
-            print('fizzbuzz')
-        else :     
-            if i % 3 == 0 :
-                print('fizz')
-            elif i % 5 == 0 :
-                print('buzz')
-            else : 
-                print(i)
+        if i % 3 == 0 :
+            print('fizz')
+        elif i % 5 == 0 :
+            print('buzz')
+        else : 
+            print(i)
 
 if __name__ == "__main__" :
     fizzbuzz(15)
